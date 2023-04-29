581a133b880501b313a3887391ca96f973cd0f07
Adam-Smart <adams8632@hotmail.co.uk>
2023-04-29 04:20:19 +0100

    added var's to js

---
+++ script.js
@@ -0,0 +1,8 @@
+var welcome =document.getElementById('welcome')
+var quiz = document.getElementById('quiz')
+var options = document.getElementById('options')
+var message = document.getElementById('message')
+var summary = document.getElementById('summary')
+
+var playQuiz = document.getElementById('playQuiz')
+
