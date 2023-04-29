b75628b268f66b84d901ef979a98afbfde8238a3
Adam-Smart <adams8632@hotmail.co.uk>
2023-04-29 05:04:25 +0100

    added func to show question and add question sheet

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var quiz = document.getElementById('quiz')
 var options = document.getElementById('options')
 var message = document.getElementById('message')
 var summary = document.getElementById('summary')
+var currentQuestion = 0
 
 var playQuiz = document.getElementById('playQuiz')
 
@@ -13,5 +14,22 @@ function startGame(){
     playQuiz.classList.add('hide')
     welcome.classList.add('hide')
     quiz.classList.remove('hide')
-    displayQuestion();
+    showQuestion();
 }
+function showQuestion(){
+    var question = questions[currentQuestion];
+    document.getElementById('question').textContent = question.title
+    currentQuestion++;
+        if(currentQuestion >= questions.length) {
+            endGame();
+            return;
+        }
+        for (var i = 0; i < question.choices.length; i++) {
+            var option = document.createElement('li');
+            option.textContent = question.choices[i];
+            option.onclick= selectAnswer;
+            option.classList.add('option')
+            options.appendChild(option);
+        }
+        
+    }
